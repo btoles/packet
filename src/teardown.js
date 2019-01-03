811638725a2799961a5a12c35693e8856c00afc6
btoles <brennan.toles@gmail.com>
2019-01-02 21:55:12 -0600

    Fix: Use different iteration, .forEach was not working properly

--- src/teardown.js
+++ src/teardown.js
@@ -9,9 +9,13 @@ const createdDevices = JSON.parse(fs.readFileSync('./src/devices.json'));
 (async () => {
     const numberOfDevices = createdDevices.devices.length;
     if (numberOfDevices > 0) {
-        createdDevices.devices.forEach(async device => {
-            await packetService.deleteDevice(device.id);
-        });
+        for (device of createdDevices.devices) {
+            const status = await packetService.deleteDevice(device.id);
+            if (status === false) {
+                console.log('Unable to delete devices.');
+                process.exit(1);
+            }
+        };
         console.log(`Successfully deleted ${numberOfDevices} devices!`);
 
         // Reset our device list.

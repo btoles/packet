e43e355656da8f0c46cd65976533a72846346583
btoles <brennan.toles@gmail.com>
2019-01-02 19:43:38 -0600

    Feature: Delete created devices

---
+++ teardown.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+require('dotenv').config();
+
+const PacketService = require('./services/packet.service');
+
+const packetService = new PacketService();
+const createdDevices = JSON.parse(fs.readFileSync('./devices.json'));
+
+(async () => {
+    const numberOfDevices = createdDevices.devices.length;
+    if (numberOfDevices > 0) {
+        createdDevices.devices.forEach(async device => {
+            await packetService.deleteDevice(device.id);
+        });
+        console.log(`Successfully deleted ${numberOfDevices} devices!`);
+
+        // Reset our device list.
+        fs.writeFileSync('./devices.json', JSON.stringify({ devices: [] }, null, 4));
+
+        process.exit(0);
+    }
+    else {
+        console.log('No devices to delete.');
+        process.exit(1);
+    }
+})();
